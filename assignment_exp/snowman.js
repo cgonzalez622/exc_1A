8f4be2a6fa721bd935db6e208f463aa9345e9e73
Camille <c.gonzalez0017@gmail.com>
2023-01-22 14:35:32 -0600

    added background image and user interaction

--- assignment_exp/snowman.js
+++ assignment_exp/snowman.js
@@ -1,10 +1,10 @@
 let grid = undefined;
-//let stroke1 = prompt("enter a basic color name in lower case", "gray");
-//let stroke2 = prompt("enter another basic color name in lower case", "purple");
+let stroke1 = prompt("enter a basic color name in lower case", "gray");
+let stroke2 = prompt("enter another basic color name in lower case", "purple");
 function setup() {
     createCanvas(1000, 800);
-    background("#ccc");
-    grid = loadImage("images/100px_grid.png");
+    background("#ffffff");
+    grid = loadImage("images/snow.png");
     
 }
 function draw() {
@@ -12,7 +12,7 @@ function draw() {
     // snowman legs
     fill("#f1f1f1");
     strokeWeight(20);
-    stroke(0);
+    stroke(stroke1);
     // bottom body
     ellipse(500, 475, 350, 350);
     // body
@@ -23,7 +23,7 @@ function draw() {
     triangle(470, 150, 375, 125, 400, 200);
     triangle(530, 150, 625, 125, 600, 200);
     // eyes
-    stroke(0);
+    stroke(stroke2);
     strokeWeight(30);
     point(460, 230);
     point(540, 230);
@@ -37,10 +37,19 @@ function draw() {
     arc(479, 275, 500, 50, .5, HALF_PI);
     line(500, 275, 500, 300);
     // tail
-    noFill();
     strokeWeight(20);
     line(150, 400, 325, 500);
     line(225, 400, 250, 450);
+    // coloring
+    if (mouseIsPressed) {
+        noStroke();
+        fill(0, 126, 255);
+    }   
+    else {
+        noFill();
+        noStroke();
+    }
+    ellipse(mouseX, mouseY, 80, 80);
     
     
     
