e6c6808b534d52f093442dda4061a138ac4330f6
Camille <c.gonzalez0017@gmail.com>
2023-01-22 14:05:34 -0600

    added cat snowman and png

--- assignment_exp/snowman.js
+++ assignment_exp/snowman.js
@@ -1,11 +1,48 @@
 let grid = undefined;
+//let stroke1 = prompt("enter a basic color name in lower case", "gray");
+//let stroke2 = prompt("enter another basic color name in lower case", "purple");
 function setup() {
     createCanvas(1000, 800);
     background("#ccc");
-    
- 
+    grid = loadImage("images/100px_grid.png");
     
 }
 function draw() {
     background(grid);
+    // snowman legs
+    fill("#f1f1f1");
+    strokeWeight(20);
+    stroke(0);
+    // bottom body
+    ellipse(500, 475, 350, 350);
+    // body
+    ellipse(500, 425, 240, 425);
+    // head
+    ellipse(500, 250, 200);
+    // hat body
+    triangle(470, 150, 375, 125, 400, 200);
+    triangle(530, 150, 625, 125, 600, 200);
+    // eyes
+    stroke(0);
+    strokeWeight(30);
+    point(460, 230);
+    point(540, 230);
+    // nose
+    stroke(0);
+    strokeWeight(10);
+    triangle(500, 275, 510, 260, 490, 260);
+    // mouth
+    noFill();
+    strokeWeight(10);
+    arc(479, 275, 500, 50, .5, HALF_PI);
+    line(500, 275, 500, 300);
+    // tail
+    noFill();
+    strokeWeight(20);
+    line(150, 400, 325, 500);
+    line(225, 400, 250, 450);
+    
+    
+    
+
 }
