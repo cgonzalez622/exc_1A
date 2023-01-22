5709f2decae40ea6507f797eb0f2b88a8928e295
Camille <c.gonzalez0017@gmail.com>
2023-01-22 12:24:22 -0600

    deleted and added img

--- assignment_exp/snowman.js
+++ assignment_exp/snowman.js
@@ -2,7 +2,7 @@ let grid = undefined;
 function setup() {
     createCanvas(1000, 800);
     background("#ccc");
-    grid = loadImage("assignment_exp/image/100px_grid.png");
+ 
     
 }
 function draw() {
